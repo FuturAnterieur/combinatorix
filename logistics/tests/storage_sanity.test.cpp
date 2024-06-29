fa99a42f350440c40a1dcfbbc1f87f165f209326
Futur Antérieur <futuranterieur7@gmail.com>
2024-06-29 14:46:12 -0400

    Twenty-first commit : more serialization experiments

--- logistics/tests/storage_sanity.test.cpp
+++ logistics/tests/storage_sanity.test.cpp
@@ -12,11 +12,68 @@ struct has_name {
   std::string Name;
 };
 
+struct wizard_info {
+  entt::entity Entity;
+  std::string Description;
+  bool is_updating{false};
+};
+
+using wizards_info = std::vector<wizard_info>;
+
+constexpr auto param_hash = entt::hashed_string::value("description");
+constexpr auto param_updated_hash = entt::hashed_string::value("description_updated");
+
 template<typename Archive>
 void serialize(Archive &archive, parameter &param){
   archive(param.DT, param.Value);
 }
 
+void serialize_round_trip(entt::registry &registry, entt::continuous_loader &loader){
+  std::stringstream data_store;
+  {
+    cereal::BinaryOutputArchive output{data_store};
+    entt::snapshot{registry}
+      .get<entt::entity>(output)
+      .get<parameter>(output, param_updated_hash);
+  }
+
+  cereal::BinaryInputArchive input{data_store};
+  loader.get<entt::entity>(input)
+        .get<parameter>(input, param_updated_hash);
+}
+
+void update_wizards(entt::registry &registry, wizards_info &info){
+  info.clear();
+  auto &&my_update_storage = registry.storage<parameter>(param_updated_hash);
+  auto &&my_stable_storage = registry.storage<parameter>(param_hash);
+  for(size_t i = 0; i < my_update_storage.size(); i++ ){
+    //Update our wizards info
+    auto entity = my_update_storage.data()[i];
+    parameter &update_param = my_update_storage.get(entity);
+    //Update registry stable data
+    parameter *stable_param{nullptr};
+    if(my_stable_storage.contains(entity)){
+      stable_param = &my_stable_storage.get(entity);
+    } else {
+      stable_param = &my_stable_storage.emplace(entity);
+    }
+    stable_param->DT = update_param.DT;
+    stable_param->Value = update_param.Value;
+  }  
+  for(size_t i = 0; i < my_stable_storage.size(); i++){
+    auto entity = my_stable_storage.data()[i];
+    parameter &stable_param = my_stable_storage.get(entity);
+    info.push_back(wizard_info{entity, stable_param.Value});
+  }
+}
+
+void compare_wizards(wizards_info &wizards1, wizards_info &wizards2){
+  CHECK(wizards1.size() == wizards2.size());
+  for(size_t i = 0; i < wizards1.size(); i++){
+    CHECK(wizards1[i].Entity == wizards2[i].Entity);
+    CHECK(wizards2[i].Description == wizards2[i].Description);
+  }
+}
 
 TEST_CASE("Storage sanity"){
   entt::registry registry;
@@ -65,51 +122,44 @@ TEST_CASE("Storage sanity"){
   
 
   //Using storage.data()
-  auto param_updated_hash = entt::hashed_string::value("description_updated");
-
-  auto &&my_param_storage = registry.storage<parameter>(param_updated_hash);
-  my_param_storage.emplace(mermelionos, data_type::string, "Le roi des paranoiaques");
-  my_param_storage.emplace(taliesin, data_type::string, "Celui qui a ecrit le Livre");
-  my_param_storage.emplace(merlin, data_type::string, "en vacances avec viviane");
+  
+  auto &&my_update_storage = registry.storage<parameter>(param_updated_hash);
+  my_update_storage.emplace(mermelionos, data_type::string, "Le roi des paranoiaques");
+  my_update_storage.emplace(taliesin, data_type::string, "Celui qui a ecrit le Livre");
+  my_update_storage.emplace(merlin, data_type::string, "en vacances avec viviane");
   
   //Beware of order!
-  std::vector<parameter> wizards_data;
-  std::vector<entt::entity> wizards;
-  for(size_t i = 0; i < my_param_storage.size(); i++ ){
-    wizards.push_back(my_param_storage.data()[i]);
-    wizards_data.push_back(my_param_storage.get(wizards[i]));
-  }  
-
-  CHECK(my_param_storage.get(wizards[2]).Value == wizards_data[2].Value);
-
-  //COPYING REGISTRY PARTS
-  std::stringstream data_store;
+  wizards_info wizards1;
+  update_wizards(registry, wizards1);
   
-
-  {
-    cereal::BinaryOutputArchive output{data_store};
-    entt::snapshot{registry}
-      .get<entt::entity>(output)
-      .get<parameter>(output, param_updated_hash);
-  }
-
-  cereal::BinaryInputArchive input{data_store};
+  //COPYING REGISTRY PARTS
   entt::registry registry2;
   entt::continuous_loader loader(registry2);
+  serialize_round_trip(registry, loader);
 
-  loader.get<entt::entity>(input)
-        .get<parameter>(input, param_updated_hash);
-
-  auto &&my_param_storage2 = registry2.storage<parameter>(param_updated_hash);
-  std::vector<parameter> wizards_data2;
-  std::vector<entt::entity> wizards2;
-  for(size_t i = 0; i < my_param_storage2.size(); i++ ){
-    wizards2.push_back(my_param_storage.data()[i]);
-    wizards_data2.push_back(my_param_storage.get(wizards2[i]));
-    CHECK(wizards_data2[i].Value == wizards_data[i].Value);
-    CHECK(wizards2[i] == wizards[i]);
-  }  
-
+  wizards_info wizards2;
+  update_wizards(registry2, wizards2);
+  compare_wizards(wizards1, wizards2);
   
+  //auto sorcier2_only = registry2.create();
+  auto sorcier1_only = registry.create();
+  my_update_storage.clear();
+  my_update_storage.emplace(sorcier1_only, data_type::string, "Le nouveau-venu");
+  
+  update_wizards(registry, wizards1);
+
+  serialize_round_trip(registry, loader);
+  update_wizards(registry2, wizards2);
+
+  compare_wizards(wizards1, wizards2);
+
+  my_update_storage.clear();
+  my_update_storage.emplace(taliesin, data_type::string, "Entité 1 avec nouvelle desc");
+  update_wizards(registry, wizards1);
+
+  serialize_round_trip(registry, loader);
+  update_wizards(registry2, wizards2);
+
+  compare_wizards(wizards1, wizards2);
 }
 
