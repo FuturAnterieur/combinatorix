b533e3f86122df09c6167fd6d6a80c85db389208
Futur Antérieur <futuranterieur7@gmail.com>
2024-07-23 20:05:14 -0400

    93rd commit : in this commit, the example is an isometric projection example

--- graphics/example.cpp
+++ graphics/example.cpp
@@ -18,6 +18,7 @@ struct params_t {
 
 int main() {
 
+    constexpr int num_instances = 2;
     // create window and GL context via GLFW
     glfw_desc_t glfw_desc = {};
     glfw_desc.title = "quad-glfw.c";
@@ -37,11 +38,10 @@ int main() {
     // create a vertex buffer
     float vertices[] = {
         // positions            colors
-        -0.5f,  0.5f, 0.5f,     1.0f, 0.0f, 0.0f, 1.0f,
-         0.5f,  0.5f, 0.5f,     0.0f, 1.0f, 0.0f, 1.0f,
-         0.5f, -0.5f, 0.5f,     0.0f, 0.0f, 1.0f, 1.0f,
-        -0.5f, -0.5f, 0.5f,     1.0f, 1.0f, 0.0f, 1.0f,
-         0.0f, -1.0f, 0.5f,     1.0f, 1.0f, 1.0f, 1.0f
+        -0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f, 1.0f,
+         0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f, 1.0f,
+         0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f,
     };
     sg_buffer_desc vbuf_desc ={}; 
     vbuf_desc.data = SG_RANGE(vertices);
@@ -52,7 +52,6 @@ int main() {
     uint16_t indices[] = {
         0, 1, 2,    // first triangle
         0, 2, 3,    // second triangle
-        3, 2, 4
     };
     sg_buffer_desc ibuf_desc = {}; 
     ibuf_desc.type = SG_BUFFERTYPE_INDEXBUFFER; 
@@ -61,7 +60,7 @@ int main() {
     sg_buffer ibuf = sg_make_buffer(&ibuf_desc);
 
     sg_buffer_desc instance_data = {};
-    instance_data.size = 2 * sizeof(hmm_vec3);
+    instance_data.size = num_instances * sizeof(hmm_vec3);
     instance_data.usage = SG_USAGE_STREAM;
     instance_data.label = "instance_data";
 
@@ -123,28 +122,71 @@ int main() {
     float rz = 0.0f;
     float timer = 0.f;
     params_t vs_params;
-    hmm_vec3 positions[2];
-    positions[0].X = -0.5f;
-    positions[1].X = 0.5f;
-    positions[0].Z = 0.f;
-    positions[1].Z = 0.f;
+    hmm_vec3 positions[num_instances];
+    positions[0].X = 0.0f;
+    positions[0].Y = 0.0f;
+    positions[0].Z = 0.0f;
+    positions[1].X = 0.0f;
+    positions[1].Y = 0.f;
+    positions[1].Z = 0.0f;
+    
+
     while (!glfwWindowShouldClose(glfw_window())) {
         sg_pass the_pass = {};
 
-        positions[0].Y = HMM_SinF(timer);
-        positions[1].Y = -HMM_SinF(timer);
         
-        hmm_mat4 proj = HMM_Perspective(60.0f, (float)glfw_width()/(float)glfw_height(), 0.01f, 10.0f);
-        hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 1.5f, 6.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
-        hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+        
+        /*float length = HMM_SQRTF(1.f/3.f);
+
+        hmm_vec3 eye = HMM_Vec3(length, length, length);
+        hmm_vec3 center = HMM_Vec3(0.0f, 0.0f, 0.0f);
+        hmm_vec3 up = HMM_Vec3(0.f, 1.f, 0.f);*/
+        //hmm_mat4 proj = HMM_Perspective(60.0f, (float)glfw_width()/(float)glfw_height(), 0.01f, 10.0f);
+        //hmm_mat4 view = HMM_LookAt(eye, center, HMM_Vec3(0.0f, 1.0f, 0.f));
+        //hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+
+        hmm_mat4 iso = {0};
+
+        //hmm_vec3 F = HMM_NormalizeVec3(HMM_SubtractVec3(center, eye));
+        //hmm_vec3 S = HMM_NormalizeVec3(HMM_Cross(F, up));
+        //hmm_vec3 U = HMM_Cross(S, F);
+
+        /*float factor = sqrtf(1/6.f);
+        iso.Elements[0][0] = sqrtf(3) * factor;
+        iso.Elements[0][1] = 0.0f;
+        iso.Elements[0][2] = -sqrtf(3.f) * factor;
+
+        iso.Elements[1][0] = 1.f * factor;
+        iso.Elements[1][1] = 2.f * factor;
+        iso.Elements[1][2] = 1.f * factor;
+
+        iso.Elements[2][0] = sqrtf(2.f) * factor;
+        iso.Elements[2][1] = -sqrtf(2.f) * factor;
+        iso.Elements[2][2] = sqrtf(2.f) * factor;
+
+        iso.Elements[3][3] = 1.0f;*/
+
+        hmm_mat4 iso1 = HMM_Rotate(45.f, HMM_Vec3(0.f, 0.f, 1.f));
+        hmm_mat4 iso2 = HMM_Rotate(35.264f, HMM_Vec3(1.f, 0.f, 0.f));
+        iso = HMM_MultiplyMat4(iso2, iso1);
+        iso.Elements[3][3] = 1.f;
+
+        hmm_mat4 ortho = {0};
+        ortho.Elements[0][0] = 1.f;
+        ortho.Elements[1][1] = 1.f;
+        ortho.Elements[2][2] = 1.f;
+        ortho.Elements[3][3] = 1.f;
+
+        hmm_mat4 ortho_iso = HMM_MultiplyMat4(ortho, iso);
 
         // rotated model matrix
-        rz += 1.0f;
+        rz += 0.f;
         hmm_mat4 model = HMM_Rotate(rz, HMM_Vec3(0.0f, 0.0f, 1.0f));
-        
+        hmm_mat4 scale = HMM_Scale(HMM_Vec3(0.5f, 0.5f, 0.5f));
+        hmm_mat4 model_scale = HMM_MultiplyMat4(model, scale);
         // model-view-projection matrix for vertex shader
-        vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
-        sg_range pos_buffers = sg_range{positions, 2 * 3 * sizeof(float)};
+        vs_params.mvp = HMM_MultiplyMat4(ortho_iso, model_scale);
+        sg_range pos_buffers = sg_range{positions, num_instances * 3 * sizeof(float)};
 
         sg_update_buffer(bind.vertex_buffers[1], &pos_buffers);
 
@@ -154,7 +196,7 @@ int main() {
         sg_apply_pipeline(pip);
         sg_apply_bindings(&bind);
         sg_apply_uniforms(SG_SHADERSTAGE_VS, 0, &uniforms);
-        sg_draw(0, 9, 2);
+        sg_draw(0, 9, num_instances);
         sg_end_pass();
         sg_commit();
         glfwSwapBuffers(glfw_window());
