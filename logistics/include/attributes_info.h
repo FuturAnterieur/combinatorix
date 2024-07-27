adeb085b042246d28c6d414d2a7624a0ba70cf85
Futur Antérieur <futuranterieur7@gmail.com>
2024-07-26 22:25:10 -0400

    101rst commit : ideation for refactor of diffs/priority
    ofc this does not compile yet

--- logistics/include/attributes_info.h
+++ logistics/include/attributes_info.h
@@ -53,7 +53,7 @@ struct logistics_API parameter {
   const parameter_value_t &value() const;
   data_type dt() const;
   void set_value(parameter_value_t value);
-  
+
 private:
   friend class cereal::access;
   data_type &access_data_type(); 
@@ -69,17 +69,6 @@ private:
   pimpl *_Pimpl;
 };
 
-struct attributes_info_snapshot {
-  std::set<entt::id_type> StatusHashes;
-  std::map<entt::id_type, parameter> ParamValues;
-};
-
-struct attributes_info_reference {
-  attributes_info_reference(std::set<entt::id_type> &stat, std::map<entt::id_type, parameter> &params) : StatusHashes(stat), ParamValues(params) {}
-  attributes_info_reference(attributes_info_snapshot &snapshot) : StatusHashes(snapshot.StatusHashes), ParamValues(snapshot.ParamValues) {}
-  std::set<entt::id_type> &StatusHashes;
-  std::map<entt::id_type, parameter> &ParamValues;
-};
 
 //integrating this to attributes_info (with accessor functions) would be more work, but would allow much easier contradicting change detection.
 //so maybe future TO-DO
@@ -93,56 +82,87 @@ struct status_t {
   using diff_t = smt;
 };
 
-struct attributes_info_changes{ 
-  std::map<entt::id_type, smt> ModifiedStatuses; 
-  std::map<entt::id_type, std::pair<parameter, parameter>> ModifiedParams;
+template<typename T>
+struct Change {
+  typename T::diff_t Diff;
+  entt::entity CommitterId;
 };
 
+template<typename T>
+struct CommittedValue {
+  T Value;
+  entt::entity CommitterId;
+  void apply_change(const Change<T> &change);
+};
+
+struct attributes_info_snapshot {
+  std::map<entt::id_type, CommittedValue<status_t>> StatusHashes;
+  std::map<entt::id_type, CommittedValue<parameter>> ParamValues;
+};
+
+
+
+/*struct attributes_info_reference {
+  attributes_info_reference(std::set<entt::id_type> &stat, std::map<entt::id_type, parameter> &params) : StatusHashes(stat), ParamValues(params) {}
+  attributes_info_reference(attributes_info_snapshot &snapshot) : StatusHashes(snapshot.StatusHashes), ParamValues(snapshot.ParamValues) {}
+  std::set<entt::id_type> &StatusHashes;
+  std::map<entt::id_type, parameter> &ParamValues;
+};*/
+
+
+
+struct attributes_info_changes{ 
+  std::map<entt::id_type, Change<status_t>> ModifiedStatuses; 
+  std::map<entt::id_type, std::pair<CommittedValue<parameter>, CommittedValue<parameter>>> ModifiedParams;
+};
+
+//sent to the backend by the clients, who should not have to care about committer IDs
 struct logistics_API  attributes_info_short_changes {
   std::map<entt::id_type, smt> ModifiedStatuses; 
   std::map<entt::id_type, parameter> ModifiedParams;
 };
 
 logistics_API attributes_info_short_changes short_changes_from_changes(const attributes_info_changes &changes);
-logistics_API bool paste_attributes_changes(const attributes_info_short_changes &changes, attributes_info_reference &ref);
+logistics_API bool paste_attributes_changes(const attributes_info_short_changes &changes, attributes_info_snapshot &snapshot);
 
-struct attributes_info_state_at_timing {
+/*struct attributes_info_state_at_timing {
   attributes_info_short_changes Changes;
   entt::entity OriginatingEntity;
-};
+};*/
 
-namespace logistics{
+/*namespace logistics{
   class change_merger;
-}
+}*/
 
 template<typename T>
-struct Change {
-  typename T::diff_t Diff;
-  entt::entity CommiterId;
-};
+  class diff_merger {
+    virtual Change<T> merge_changes(const Change<T> &left, const Change<T> &right, const priority_callback_t &prio_func);
+  };
 
 template<typename T>
 struct generic_history {
   std::map<timing_t, Change<T>> History;
+  void add_change(timing_t timing, const typename T::diff_t &diff, const priority_callback_t &prio_func);
+  void cumulative_changes(timing_t upper_bound, const priority_callback_t &prio_func);
 };
 
 struct attributes_info_history {
   const attributes_info_snapshot StartingPoint;
-  std::map<timing_t, attributes_info_state_at_timing> History;
+  //std::map<timing_t, attributes_info_state_at_timing> History;
 
   std::map<entt::id_type, generic_history<status_t>> StatusesHistory2; //DA FUTURE
   std::map<entt::id_type, generic_history<parameter>> ParamsHistory2;
 
-  bool add_changes(timing_t timing, const attributes_info_state_at_timing &changes, const priority_callback_t &callback, void *cb_user_data);
-  bool add_changes_2(timing_t timing, const attributes_info_short_changes &changes, entt::entity originating_entity, const priority_callback_t &callback, void *cb_user_data);
+  //bool add_changes(timing_t timing, const attributes_info_state_at_timing &changes, const priority_callback_t &callback, void *cb_user_data);
+  bool add_changes(timing_t timing, const attributes_info_short_changes &changes, entt::entity originating_entity, const priority_callback_t &callback, void *cb_user_data);
   //only one commiter per add_changes call???
   //unless short_changes also contains commiter info???
   //to be ascertained in refactor Volume II
 
   attributes_info_snapshot produce_snapshot(timing_t upper_bound = std::numeric_limits<timing_t>::max()) const;
-  bool cumulative_changes(attributes_info_short_changes &cumul_changes, timing_t upper_bound = std::numeric_limits<timing_t>::max()) const;
-  bool cumulative_changes_disregarding_timing(attributes_info_short_changes &cumul_changes, timing_t lower_bound, timing_t upper_bound) const;
-  bool cumulative_changes_swiss_knife(attributes_info_short_changes &cumul_changes, timing_t lower_bound, timing_t upper_bound, logistics::change_merger *merger) const;
+  //bool cumulative_changes(attributes_info_short_changes &cumul_changes, timing_t upper_bound = std::numeric_limits<timing_t>::max()) const;
+  //bool cumulative_changes_disregarding_timing(attributes_info_short_changes &cumul_changes, timing_t lower_bound, timing_t upper_bound) const;
+  //bool cumulative_changes_swiss_knife(attributes_info_short_changes &cumul_changes, timing_t lower_bound, timing_t upper_bound, logistics::change_merger *merger) const;
 };
 
 logistics_API bool changes_empty(attributes_info_changes &changes);
