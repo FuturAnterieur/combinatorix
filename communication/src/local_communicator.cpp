33866bbe3b48db3dbac27676f4e894f7f729ea3c
Futur Antérieur <futuranterieur7@gmail.com>
2024-08-11 22:47:44 -0400

    146th commit : remove receiverAck again - this time it's more promising

--- communication/src/local_communicator.cpp
+++ communication/src/local_communicator.cpp
@@ -11,7 +11,7 @@ void local_communicator::send(size_t channel_id, const std::string &data, bool w
   local_sync_channel &channel = *Container->get_channel(channel_id);
   std::unique_lock lk(channel.Mutex);
   
-  if(wait_before) channel.cvSendCounter.wait(lk, [&channel](){return channel.SendCount <= channel.ReceiveCount; });
+  if(wait_before) channel.cvSendCounter.wait(lk, [&channel](){return channel.CompletionCount >= channel.ReceiveCount; });
 
   channel.InfoReady = true;
   channel.Data = data;
@@ -19,8 +19,9 @@ void local_communicator::send(size_t channel_id, const std::string &data, bool w
   channel.SendCount++;
   channel.cvWaiting.notify_one();
 
-  channel.ReceiverAck = false;
-  if(wait_after) channel.cvReceiverAck.wait(lk, [&channel](){return channel.ReceiverAck; });
+  if(wait_after) channel.cvSendCounter.wait(lk, [&channel](){return channel.ReceiveCount >= channel.SendCount; });
+  channel.CompletionCount++;
+  channel.cvSendCounter.notify_all();
 
   lk.unlock();
 }
@@ -36,10 +37,7 @@ void local_communicator::receive(size_t channel_id, std::string &data){
   data = channel.Data; //use move???
   channel.InfoReady = false;
   channel.ReceiveCount++;
-  channel.cvSendCounter.notify_one();
-
-  channel.ReceiverAck = true;
-  channel.cvReceiverAck.notify_one();
+  channel.cvSendCounter.notify_all();
 
   lk.unlock();
 }
